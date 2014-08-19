e3c93207439fb024e0fc3dd7fbb688a0aa544b94
Binny V A <binnyva@gmail.com>
2014-08-19 18:09:26 +0530

    Added already recuited people count to National Requirements

--- national_requirements.php
+++ national_requirements.php
@@ -7,8 +7,14 @@ $crud->title = 'National Volunteer Requirement';
 
 $crud->addField('cycle', 'Cycle', 'int', array(), $all_cycles, 'select');
 $crud->addListDataField('city_id', 'City', 'City');
-$crud->addListDataField('added_by_user_id', 'User', 'Added By', "user_type='volunteer' AND status='1'");
-$crud->setListingFields('city_id','vertical_id','requirement_count','cycle','remarks','added_on','added_by_user_id');
+$crud->addField('recruited_count', 'People Recruited', 'virtual', array(), array(
+	'sql'=>"SELECT COUNT(U.id) FROM User U
+				INNER JOIN UserGroup UG ON UG.user_id=U.id 
+				INNER JOIN `Group` G ON G.id=UG.group_id
+				WHERE G.vertical_id='%vertical_id%' AND U.joined_on >= '%added_on%' AND U.city_id='%city_id%'"));
+
+//$crud->addListDataField('added_by_user_id', 'User', 'Added By', "user_type='volunteer' AND status='1'");
+$crud->setListingFields('city_id','vertical_id','requirement_count','recruited_count','cycle','remarks','added_on'); //,'added_by_user_id');
 
 $crud->setListingQuery("SELECT * FROM HR_Volunteer_Request ORDER BY city_id");
 
--- volunteer_engagement.php
+++ volunteer_engagement.php
@@ -27,7 +27,7 @@ if($current_user['fellow'] == 'hr') {
 	$crud->setListingQuery("SELECT * FROM HR_Engagement WHERE city_id=$city_id WHERE added_by_user_id=$user_id");
 	$crud->addField('added_by_user_id', 'User', 'int', array(), $user_id, 'hidden');
 }
-$crud->setListingFields(array('month','selected_users','added_on','added_by_user_id'));
+$crud->setListingFields(array('cycle','selected_users','added_on','added_by_user_id'));
 
 $crud->title = 'Engagement Request';
 
