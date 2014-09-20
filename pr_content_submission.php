6d5498284852d3b41e598ba6dd3a8182c7259524
Binny V A <binnyva@gmail.com>
2014-09-20 15:38:54 +0530

    More bug fixes

--- pr_content_submission.php
+++ pr_content_submission.php
@@ -28,6 +28,8 @@ $crud->addField('added_on','Added On','datetime',array(),date('Y-m-d H:i:s'));
 $crud->addField('status','Status','varchar',array(),$all_status,'select');
 
 //$QUERY['status_updated_on'] = date('Y-m-d H:i:s');
+if($region_id == 0) $cities_in_region = array_keys($all_cities);
+else $cities_in_region = $sql->getCol("SELECT id FROM City WHERE region_id=$region_id");
 
 // If Intern
 if($user_type == 'intern') {
@@ -35,21 +37,16 @@ if($user_type == 'intern') {
 	$crud->addField("intern_user_id", 'Intern', 'int', array(), $user_id, 'hidden');
 
 } elseif($user_type == 'fellow') {
-	$crud->setListingQuery("SELECT * FROM PR_Content PRC INNER JOIN User U ON U.id=PRC.user_id WHERE U.city_id=$city_id"); // Show only their own city
-	$crud->addListDataField('intern_user_id', 'User', 'Intern', "city_id=$city_id AND user_type='volunteer' AND status='1'");
-
-} elseif($user_type == 'strat') {
-	$crud->setListingQuery("SELECT * FROM PR_Content PRC INNER JOIN User U ON U.id=PRC.user_id INNER JOIN City ON City.id=User.city_id WHERE City.region_id=$region_id"); // Show only their own region
-
-	if($region_id == 0) $cities_in_region = array_keys($all_cities);
-	else $cities_in_region = $sql->getCol("SELECT id FROM City WHERE region_id=$region_id");
-	$crud->addListDataField('intern_user_id', 'User', 'Intern', "city_id IN (".implode(',', $cities_in_region).") AND user_type='volunteer' AND status='1'");
+	$crud->setListingQuery("SELECT * FROM PR_Content PRC INNER JOIN User U ON U.id=PRC.intern_user_id WHERE U.city_id=$city_id"); // Show only their own city
+	$crud->addField('intern_user_id', "Intern", 'enum', array(), $sql->getById("SELECT U.id,U.name 
+			FROM User U INNER JOIN UserGroup UG ON UG.user_id=U.id INNER JOIN `Group` G ON G.id=UG.group_id 
+			WHERE U.city_id=$city_id AND U.user_type='volunteer' AND U.status='1' AND G.vertical_id=7"));
 
 } else {
-	$crud->setListingQuery("SELECT * FROM PR_Content PRC INNER JOIN User U ON U.id=PRC.user_id INNER JOIN City ON City.id=User.city_id"); // Show only their own region
-
-	$cities_in_region = $sql->getCol("SELECT id FROM City WHERE region_id=$region_id");
-	$crud->addListDataField('intern_user_id', 'User', 'Intern', "city_id IN (".implode(',', $cities_in_region).") AND user_type='volunteer' AND status='1'");
+	$crud->setListingQuery("SELECT * FROM PR_Content PRC INNER JOIN User U ON U.id=PRC.intern_user_id INNER JOIN City ON City.id=U.city_id WHERE City.region_id=$region_id"); // Show only their own region
+	$crud->addField('intern_user_id', "Intern", 'enum', array(), $sql->getById("SELECT U.id,U.name 
+			FROM User U INNER JOIN UserGroup UG ON UG.user_id=U.id INNER JOIN `Group` G ON G.id=UG.group_id
+			WHERE U.city_id IN (".implode(',', $cities_in_region).") AND U.user_type='volunteer' AND U.status='1' AND G.vertical_id=7"));
 }
 
 render('crud.php');
--- pr_requirement.php
+++ pr_requirement.php
@@ -20,8 +20,6 @@ $all_status = array(
 	'approved'		=> "Approved",
 	'done'			=> "Done"
 	);
-$all_cities = $sql->getById("SELECT id,name FROM City");
-
 $crud->addField('type','Type','varchar',array(),$all_types,'select');
 $crud->addField('added_on','Added On','datetime',array(),date('Y-m-d H:i:s'));
 $crud->addField('status','Status','varchar',array(),$all_status,'select');
@@ -29,26 +27,31 @@ $crud->addField('status','Status','varchar',array(),$all_status,'select');
 if($user_type == 'fellow') {
 	$crud->setListingQuery("SELECT * FROM PR_Requirement PRR INNER JOIN User ON User.id=PRR.user_id WHERE User.city_id=$city_id"); // Show submission from their city
 
-} elseif($user_type == 'strat') {
+} elseif($user_type == 'strat' or $user_type == 'national') {
 	$crud->setListingQuery("SELECT * FROM PR_Requirement PR INNER JOIN User U ON U.id=PRC.user_id INNER JOIN City ON City.id=User.city_id WHERE City.region_id=$region_id"); // Show only their own region
 
 	if($region_id == 0) $cities_in_region = array_keys($all_cities);
 	else $cities_in_region = $sql->getCol("SELECT id FROM City WHERE region_id=$region_id");
+	$cities_in_region = $sql->getCol("SELECT id FROM City WHERE region_id=$region_id");
 	$crud->addListDataField('added_by_user_id', 'User', 'Added By', "city_id IN (".implode(',', $cities_in_region).") AND user_type='volunteer' AND status='1'");
 }
+
+
+// When adding, hide unnessary fields
 if(i($QUERY, 'action') == 'add') {
 	$crud->addField("added_by_user_id", 'Added By', 'int', array(), $user_id, 'hidden');
 	$crud->addField("promised_on", 'Requried', 'date', array(), '0000-00-00', 'hidden');
 	$crud->addField("delivered_on", 'Requried', 'date', array(), '0000-00-00', 'hidden');
 
+// Send email when things are added.
 } elseif(i($QUERY, 'action') == 'add_save') {
 	$pr_fellow = $sql->getAssoc("SELECT U.* FROM User U INNER JOIN UserGroup UG ON U.id=UG.user_id INNER JOIN `Group` G ON G.id=UG.group_id WHERE G.vertical_id=7 AND G.type='fellow' AND U.status='1' AND U.user_type='volunteer'");
 	$pr_strat = $sql->getAssoc("SELECT U.* FROM User U INNER JOIN UserGroup UG ON U.id=UG.user_id INNER JOIN `Group` G ON G.id=UG.group_id WHERE G.vertical_id=7 AND G.type='strat' AND U.status='1' AND U.user_type='volunteer'");
 	$all_verticals = $sql->getById("SELECT id,name FROM Vertical");
-	
-	$all_verticals[0] = 'None';
+	$all_cities = $sql->getById("SELECT id,name FROM City");
 
-	//$pr_fellow['email'] = 'binnyva@makeadiff.in'; $pr_strat['email'] = 'cto@makeadiff.in';
+	// $pr_fellow['email'] = 'binnyva@makeadiff.in';
+	// $pr_strat['email'] = 'cto@makeadiff.in';
 
 	$message = <<<END
 Hey %NAME%,
@@ -66,7 +69,7 @@ MAD Tech
 END;
 
 	$replaces = array(
-		'%VERTICAL%'=> $all_verticals[$user['vertical_id']],
+		'%VERTICAL%'	=> $all_verticals[$user['vertical_id']],
 		'%TYPE%'	=> $user['group_type'],
 		'%CITY%'	=> $all_cities[$user['city_id']],
 		'%TITLE%'	=> $QUERY['name'],
