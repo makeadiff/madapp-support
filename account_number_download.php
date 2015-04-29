afa41cd0eb8663149ada151e41ed7c627f8587d5
Binny V A <binnyva@gmail.com>
2015-04-29 13:28:20 +0530

    The file be downloaded as Accounts.csv when URL is called..

--- account_number_download.php
+++ account_number_download.php
@@ -1,6 +1,9 @@
 <?php
 require('./common.php');
 
+header("Content-type: text/csv");
+header('Content-Disposition: attachment; filename="Accounts.csv"');
+
 $data = $sql->getAll("SELECT U.id,U.name, U.email, D.data FROM `UserData` D INNER JOIN User U ON U.id=D.user_id WHERE D.name='bank_account_number' AND data != ''");
 $ifsc = $sql->getById("SELECT U.id,D.data AS ifsc FROM `UserData` D INNER JOIN User U ON U.id=D.user_id WHERE D.name='bank_ifsc_code' AND data != ''");
 
